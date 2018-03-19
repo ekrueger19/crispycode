e643c2b0170d21f4a7947a58fc2a5a98d1675dad
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-03-19 10:24:40 -0500

    hopefully it won't get stuck turning right anymore

--- reverse.py
+++ reverse.py
@@ -33,30 +33,31 @@ while RPL.digitalRead(16) == 1:
                                 print "stopping for a moment"
                                 while RPL.digitalRead(16) == 1:
                                     print "nothing in front"
-                                    while RPL.digitalRead(17) == 1:
-                                        print "nothing to the right"
-                                        future = time.time() + 2
-                                        while time.time() < future:
-                                            RPL.servoWrite(0, 0)
-                                            RPL.servoWrite(2, 2000)
-                                            print "turning right"
-                                            if time.time() >= future:
-                                                RPL.servoWrite(0, 2000)
-                                                RPL.servoWrite(2, 1000)
-                                                print "going forward"
-                                    while RPL.digitalRead(17) == 0:
-                                        print "something to the right"
-                                        while RPL.digitalRead(24) == 1:
-                                            print "nothing to the left"
+                                    while RPL.digitalRead(24) == 0:
+                                        while RPL.digitalRead(17) == 1:
+                                            print "nothing to the right"
                                             future = time.time() + 2
                                             while time.time() < future:
-                                                RPL.servoWrite(0, 1000)
-                                                RPL.servoWrite(2, 0)
-                                                print "turning left"
-                                                while time.time() >= future:
-                                                    RPL.servoWrite(0, 1000)
-                                                    RPL.servoWrite(2, 2000)
+                                                RPL.servoWrite(0, 0)
+                                                RPL.servoWrite(2, 2000)
+                                                print "turning right"
+                                                if time.time() >= future:
+                                                    RPL.servoWrite(0, 2000)
+                                                    RPL.servoWrite(2, 1000)
                                                     print "going forward"
+                                        while RPL.digitalRead(17) == 0:
+                                            print "something to the right"
+                                            while RPL.digitalRead(24) == 1:
+                                                print "nothing to the left"
+                                                future = time.time() + 2
+                                                while time.time() < future:
+                                                    RPL.servoWrite(0, 1000)
+                                                    RPL.servoWrite(2, 0)
+                                                    print "turning left"
+                                                    while time.time() >= future:
+                                                        RPL.servoWrite(0, 1000)
+                                                        RPL.servoWrite(2, 2000)
+                                                        print "going forward"
 
 while RPL.digitalRead(16) == 0:
     future = time.time() + 3
@@ -72,20 +73,21 @@ while RPL.digitalRead(16) == 0:
                         RPL.servoWrite(2, 1000)
                         while time.time() >= future:
                             while RPL.digitalRead(16) == 1:
-                                while RPL.digitalRead(24) == 1:
-                                    future = time.time() + 2
-                                    while time.time() < future:
-                                        RPL.servoWrite(0, 0)
-                                        RPL.servoWrite(2, 12000)
-                                        if time.time() >= future:
-                                            RPL.servoWrite(0, 1000)
-                                            RPL.servoWrite(2, 2000)
-                                while RPL.digitalRead(24) == 0:
-                                    while RPL.digitalRead(17) == 1:
+                                while RPL.digitalRead(17) == 0:
+                                    while RPL.digitalRead(24) == 1:
                                         future = time.time() + 2
                                         while time.time() < future:
-                                            RPL.servoWrite(2, 0)
-                                            RPL.servoWrite(0, 1000)
+                                            RPL.servoWrite(0, 0)
+                                            RPL.servoWrite(2, 12000)
                                             if time.time() >= future:
                                                 RPL.servoWrite(0, 1000)
                                                 RPL.servoWrite(2, 2000)
+                                    while RPL.digitalRead(24) == 0:
+                                        while RPL.digitalRead(17) == 1:
+                                            future = time.time() + 2
+                                            while time.time() < future:
+                                                RPL.servoWrite(2, 0)
+                                                RPL.servoWrite(0, 1000)
+                                                if time.time() >= future:
+                                                    RPL.servoWrite(0, 1000)
+                                                    RPL.servoWrite(2, 2000)
