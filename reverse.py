a828f70d0b27749c18caefc0c3532d14bd85d60a
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-03-21 10:34:50 -0500

    annoying tabs

--- reverse.py
+++ reverse.py
@@ -34,12 +34,12 @@ while RPL.digitalRead(16) == 1:
                 while RPL.digitalRead(24) == 0:
                     while RPL.digitalRead(17) == 1:
                         print "nothing to the right"
-                            future = time.time() + 2
-                            while time.time() < future:
-                                RPL.servoWrite(0, 0)
-                                RPL.servoWrite(2, 2000)
-                                print "turning right"
-                                if time.time() >= future:
+                        future = time.time() + 2
+                        while time.time() < future:
+                            RPL.servoWrite(0, 0)
+                            RPL.servoWrite(2, 2000)
+                            print "turning right"
+                            if time.time() >= future:
                                 RPL.servoWrite(0, 2000)
                                 RPL.servoWrite(2, 1000)
                                 print "going forward"
