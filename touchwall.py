8969baa91e5403944513b87c95e703991a09f652
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-04-03 10:04:13 -0500

    motorrrrrrrrrrrrssssssssss

--- touchwall.py
+++ touchwall.py
@@ -5,21 +5,21 @@ import time
 now = time.time()
 future = now
 close = RPL.digitalRead(16)
-motorL = 1
+motorL = 0
 motorR = 2
 
-RPL.servoWrite(0, 2000)
-RPL.servoWrite(2, 1000)
+RPL.servoWrite(0, 1000)
+RPL.servoWrite(2, 2000)
 
 while True:
     future = time.time() + 1
     while RPL.digitalRead(16) == 0:
         while time.time() < future:
-            RPL.servoWrite(motorL, 2000)
-            RPL.servoWrite(motorR, 1000)
+            RPL.servoWrite(motorL, 1000)
+            RPL.servoWrite(motorR, 2000)
         while time.time() >= future:
             RPL.servoWrite(motorR, 0)
             RPL.servoWrite(motorL, 0)
     while RPL.digitalRead(16) == 1:
-        RPL.servoWrite(0, 2000)
-        RPL.servoWrite(2, 1000)
+        RPL.servoWrite(0, 1000)
+        RPL.servoWrite(2, 2000)
