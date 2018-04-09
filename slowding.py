3e208effd58f0f4b68dbdbe9b4be9818d9e5e6bf
Annabelle Hilson <ahilson19@bsmschool.org>
2018-04-09 10:17:05 -0500

    new code

---
+++ slowding.py
@@ -0,0 +1,16 @@
+import setup
+import RoboPiLib as RPL
+import time
+
+motorR = 2
+motorL = 0
+
+while True:
+    while RPL.digitalRead(16) == 1
+        RPL.servoWrite(motorL, 1000)
+        RPL.servoWrite(motorR, 2000)
+        print "nothing"
+
+    while RPL.digitalRead(16) == 0
+        RPL.servoWrite(motorL, 500)
+        RPL.servoWrite(motorR, 500)
