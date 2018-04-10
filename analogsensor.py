7d8f845dcb4059598a6d5d12675a80929aac7097
Amelia Backes <abackes19@bsmschool.org>
2018-04-10 09:57:30 -0500

    make the analog sensor work!!!

---
+++ analogsensor.py
@@ -0,0 +1,23 @@
+# analog
+
+import setup
+import RoboPiLib as RPL
+import time
+
+now = time.time()
+future = now
+
+motorL = 0
+motorR = 2
+
+right = 17
+front = 16
+left = 19
+
+analogR = 5
+
+rgo = 2000
+lgo = 1000
+
+while True:
+    print RPL.analogRead(analogR)
