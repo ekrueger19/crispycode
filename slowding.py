8fa3a892e8c611d8417a9c8c383c4b9841ef3f98
Annabelle Hilson <ahilson19@bsmschool.org>
2018-04-10 09:56:23 -0500

    minor change, error occurred

--- slowding.py
+++ slowding.py
@@ -6,11 +6,11 @@ motorR = 2
 motorL = 0
 
 while True:
-    while RPL.digitalRead(16) == 1
+    while RPL.digitalRead(16) == 1:
         RPL.servoWrite(motorL, 1000)
         RPL.servoWrite(motorR, 2000)
         print "nothing"
 
-    while RPL.digitalRead(16) == 0
+    while RPL.digitalRead(16) == 0:
         RPL.servoWrite(motorL, 500)
         RPL.servoWrite(motorR, 500)
