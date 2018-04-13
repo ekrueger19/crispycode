f01a869313150e388a719d25068bd86f01f9d076
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-04-13 09:24:45 -0500

    weird pull thing

--- slowding.py
+++ slowding.py
@@ -7,10 +7,17 @@ motorL = 0
 
 while True:
     while RPL.digitalRead(16) == 1:
-        RPL.servoWrite(motorL, 1000)
+        RPL.servoWrite(motorL, 100)
         RPL.servoWrite(motorR, 2000)
-
+        print "nothing"
 
     while RPL.digitalRead(16) == 0:
-        RPL.servoWrite(motorL, 500)
-        RPL.servoWrite(motorR, 500)
+        future = time.time() + 0.5
+        while time.time() < future:
+            RPL.servoWrite(motorL, 600)
+            RPL.servoWrite(motorR, 1500)
+            print "slowing"
+            while time.time() >= future:
+                RPL.servoWrite(motorL, 0)
+                RPL.servoWrite(motorR, 0)
+                print "stop"
