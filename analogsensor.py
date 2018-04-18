554d9cae0619e8a1783686421d234d8885d3fd1d
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-04-18 09:55:45 -0500

    merp

--- analogsensor.py
+++ analogsensor.py
@@ -30,3 +30,5 @@ while True:
         while RPL.digitalRead(19) == 1:
             print "far"
             print RPL.analogRead(analogR)
+        while RPL.analogRead(analogR) >= 550:
+            continue
