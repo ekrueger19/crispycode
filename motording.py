56ce070ed7acc8bb5945fc96b1735ed6a3b81095
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-04-20 09:53:56 -0500

    blerp

--- motording.py
+++ motording.py
@@ -18,7 +18,18 @@ while raw_input("> ") == "a":
 while raw_input("> ") == "b":
     x = 100
     x = x + 100
+    print x
     y = 2000
     y = y - 100
+    print y
     RPL.servoWrite(lala, x)
     RPL.servoWrite(falala, y)
+while raw_input("> ") == "c":
+    food = x
+    food = food + 50
+    print food
+    ugh = y
+    ugh = ugh - 50
+    print ugh
+    RPL.servoWrite(lala, food)
+    RPL.servoWrite(falala, ugh)
