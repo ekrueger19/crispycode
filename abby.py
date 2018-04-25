3df0fe3a1889a26fa98b2bc3bfdf4b789a03096a
Amelia Backes <abackes19@bsmschool.org>
2018-04-25 10:47:41 -0500

    i am the senate

--- abby.py
+++ abby.py
@@ -14,38 +14,53 @@ lock = 800
 step1 = 1300
 step2 = 1700
 unlock = 3000
+donde = 1
 
-#need: make incriments of openness- p1 will only open a bit and p2 more etc
+# idea- make it so that if you get it wrong you go back, not just end
 
 while True:
     RPL.servoWrite(0,lock)
+    donde = 1
     print "Do you want to unlock a safe? Riddle me this:"
     print "Why don't you like sand?"
 
-    p1 = raw_input("> ")
-    if p1 == "course":
-        RPL.servoWrite(0,step1)
-    else:
-        RPL.servoWrite(0,lock)
-        print "It's over Anakin"
-        break
+    if donde = 1:
+        one()
+    if donde = 2:
+        two()
+    if donde = 3:
+        three()
 
-    p2 = raw_input("> ")
-    if p2 == "rough":
-        RPL.servoWrite(0,step2)
-    else:
-        RPL.servoWrite(0,lock)
-        print "It's over Anakin"
-        break
+    def one():
+        p1 = raw_input("> ")
+        if p1 == "course":
+            RPL.servoWrite(0,step1)
+            donde = donde + 1
+        elif p1 == "You're going down a path I can't follow"
+            print "Ironic."
+            break
+        else:
+            RPL.servoWrite(0,lock)
+            print "It's over Anakin"
+            donde = 1
 
+    def two():
+        p2 = raw_input("> ")
+        if p2 == "rough":
+            RPL.servoWrite(0,step2)
+            donde = donde + 1
+        else:
+            RPL.servoWrite(0,lock)
+            print "It's over Anakin"
+            donde = 1
 
-    p3 = raw_input("> ")
-    if p3 == "irritating":
-        RPL.servoWrite(0,unlock)
-        print "General Kenobi, you are a bold one."
-        break
-
-    else:
-        RPL.servoWrite(0,lock)
-        print "It's over Anakin"
-        break
+    def three():
+        p3 = raw_input("> ")
+        if p3 == "irritating":
+            RPL.servoWrite(0,unlock)
+            print "General Kenobi, you are a bold one."
+            break
+        else:
+            RPL.servoWrite(0,lock)
+            print "It's over Anakin"
+            donde = 1
