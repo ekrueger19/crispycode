d030044016e0e3ae23f9fb6115c55fdf617112c8
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-04-27 09:54:09 -0500

    blep

--- gap.py
+++ gap.py
@@ -13,6 +13,10 @@ motorL = 0
 # motorR forward = 1000
 # motorL forward = 2000
 
+
+# Okay so this is good but I think we need to do a thing to actually make it...
+# ...turn into a gap because right now it's just turning away from a wall
+
 RPL.servoWrite(motorR,2000)
 RPL.servoWrite(motorL,100)
 
