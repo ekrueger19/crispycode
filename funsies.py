2301eaa5b779bebe4c13238065cb8b052ad1b16c
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-06-19 12:46:32 -0400

    funsies

---
+++ funsies.py
@@ -0,0 +1,10 @@
+import webbrowser
+
+print "Hey, bro, how you doin today? Write, Read, or Memes?"
+
+if raw_input("> ") == "Write":
+    webbrowser.open('https://www.pinterest.ca/mandycorine/writing-prompts/')
+if raw_input("> ") == "Read":
+    webbrowser.open('https://www.tor.com/category/all-fiction/original-fiction/')
+if raw_input("> ") == "Memes":
+    webbrowser.open('https://www.youtube.com/results?search_query=vines')
