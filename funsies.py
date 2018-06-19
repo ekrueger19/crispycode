a45558e7079c6021565d778de1e64740bef8ec06
ekrueger19 <33067215+ekrueger19@users.noreply.github.com>
2018-06-19 12:56:35 -0400

    yes

--- funsies.py
+++ funsies.py
@@ -2,9 +2,9 @@ import webbrowser
 
 print "Hey, bro, how you doin today? Write, Read, or Memes?"
 
+if raw_input("> ") == "Memes":
+    webbrowser.open('https://cheezburger.com/3907845/27-thor-ragnarok-memes-that-are-hela-hilarious')
 if raw_input("> ") == "Write":
     webbrowser.open('https://www.pinterest.ca/mandycorine/writing-prompts/')
 if raw_input("> ") == "Read":
     webbrowser.open('https://www.tor.com/category/all-fiction/original-fiction/')
-if raw_input("> ") == "Memes":
-    webbrowser.open('https://cheezburger.com/3907845/27-thor-ragnarok-memes-that-are-hela-hilarious')
